f5f512a705f9c8fba6f6e7018aa33a1930ae018e
Mohammed Farid <dev.mohammedfarid@gmail.com>
2020-04-14 13:16:00 +0200

    typos

--- server.ts
+++ server.ts
@@ -2,4 +2,4 @@ import app from '@server/app';
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log('Server hast started on port', PORT));
+app.listen(PORT, () => console.log('Server has started on port', PORT));
